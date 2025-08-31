979a098214d9b05e1216cc16fd38cda9c3e7d7c1
Mandeep135 <mandeepsinghnz24@gmail.com>
2025-08-31 20:27:04 +1200

    changed-Mandeep

--- hotel.Designer.cs
+++ hotel.Designer.cs
@@ -41,6 +41,11 @@
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -76,7 +81,7 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.LinkColor = System.Drawing.Color.DimGray;
-            this.linkLabel1.Location = new System.Drawing.Point(357, 115);
+            this.linkLabel1.Location = new System.Drawing.Point(346, 116);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(209, 20);
             this.linkLabel1.TabIndex = 3;
@@ -130,7 +135,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::HelloAuckland.Properties.Resources.hero_auckland_jpg__1_;
-            this.pictureBox2.Location = new System.Drawing.Point(-9, -24);
+            this.pictureBox2.Location = new System.Drawing.Point(-16, -21);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(1177, 642);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -140,7 +145,7 @@
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Image = global::HelloAuckland.Properties.Resources.Hotel1;
+            this.pictureBox3.Image = global::HelloAuckland.Properties.Resources.Hotel3;
             this.pictureBox3.Location = new System.Drawing.Point(35, 343);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(291, 191);
@@ -151,7 +156,7 @@
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Image = global::HelloAuckland.Properties.Resources.Hotel1;
+            this.pictureBox4.Image = global::HelloAuckland.Properties.Resources.Hotel2;
             this.pictureBox4.Location = new System.Drawing.Point(776, 70);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(291, 191);
@@ -164,7 +169,7 @@
             // 
             this.linkLabel2.AutoSize = true;
             this.linkLabel2.LinkColor = System.Drawing.Color.DimGray;
-            this.linkLabel2.Location = new System.Drawing.Point(881, 287);
+            this.linkLabel2.Location = new System.Drawing.Point(810, 278);
             this.linkLabel2.Name = "linkLabel2";
             this.linkLabel2.Size = new System.Drawing.Size(209, 20);
             this.linkLabel2.TabIndex = 3;
@@ -176,7 +181,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(821, 282);
+            this.label5.Location = new System.Drawing.Point(780, 273);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(34, 25);
             this.label5.TabIndex = 6;
@@ -184,13 +189,69 @@
             // 
             // label6
             // 
-            this.label6.Location = new System.Drawing.Point(772, 318);
+            this.label6.Location = new System.Drawing.Point(781, 307);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(345, 43);
             this.label6.TabIndex = 4;
             this.label6.Text = "Set in a tranquil hillside location above Palm Beach, enjoy stunning ocean views " +
     "all year round";
             this.label6.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(332, 343);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "📍";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.LinkColor = System.Drawing.Color.DimGray;
+            this.linkLabel3.Location = new System.Drawing.Point(357, 348);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(209, 20);
+            this.linkLabel3.TabIndex = 3;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "4 Bay Road, Waiheke Island";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(328, 394);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(326, 73);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Set in a tranquil hillside location above Palm Beach, enjoy stunning ocean views " +
+    "all year round";
+            this.label8.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(332, 488);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "VIEW DETAILS";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(776, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(212, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "VIEW DETAILS";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form4
             // 
@@ -201,11 +262,16 @@
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel3);
             this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
@@ -239,5 +305,10 @@
         private System.Windows.Forms.LinkLabel linkLabel2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
