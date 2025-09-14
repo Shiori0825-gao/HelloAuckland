c3412ce8ded2388567e15efa7eed4780f55e2804
Mandeep135 <mandeepsinghnz24@gmail.com>
2025-09-15 04:34:47 +1200

    Changed-Mandeep

---
+++ see&do.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloAuckland
+{
+    public partial class see_do : Form
+    {
+        public see_do()
+        {
+            InitializeComponent();
+        }
+    }
+}
