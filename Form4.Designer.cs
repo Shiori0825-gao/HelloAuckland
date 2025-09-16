d7a989f9f356a5624cf6f13eb748a56bb11ddb6f
Mandeep135 <mandeepsinghnz24@gmail.com>
2025-09-16 15:15:31 +1200

    Changed-Mandeep

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -31,6 +31,7 @@ namespace HelloAuckland
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
@@ -75,40 +76,44 @@ namespace HelloAuckland
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(316, 70);
+            this.label2.Location = new System.Drawing.Point(312, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(261, 32);
+            this.label2.Size = new System.Drawing.Size(296, 32);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Marina Bay Sands";
+            this.label2.Text = "GRAND MILLENIUM ";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.linkLabel1.LinkColor = System.Drawing.Color.DimGray;
-            this.linkLabel1.Location = new System.Drawing.Point(357, 106);
+            this.linkLabel1.Location = new System.Drawing.Point(353, 110);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(208, 20);
+            this.linkLabel1.Size = new System.Drawing.Size(227, 20);
             this.linkLabel1.TabIndex = 3;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Bayfront Avenue, Singapore";
+            this.linkLabel1.Text = "Grand Millennium Auckland";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(314, 127);
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(309, 139);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(318, 85);
             this.label3.TabIndex = 4;
-            this.label3.Text = "A world-renowned luxury hotel featuring a rooftop infinity pool and iconic skypar" +
-    "k. Overlooks Singapore\'s dazzling skyline and Marina Bay.";
+            this.label3.Text = "A world-renowned luxury hotel featuring a rooftop infinity pool and iconic skycit" +
+    "y. Overlooks Auckland dazzling skyline and Marina Bay.";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.MidnightBlue;
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(309, 215);
+            this.button1.Location = new System.Drawing.Point(309, 227);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(212, 46);
             this.button1.TabIndex = 5;
@@ -120,7 +125,8 @@ namespace HelloAuckland
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(317, 102);
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(313, 106);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 25);
             this.label4.TabIndex = 6;
@@ -131,19 +137,20 @@ namespace HelloAuckland
             // 
             this.linkLabel2.AutoSize = true;
             this.linkLabel2.LinkColor = System.Drawing.Color.DimGray;
-            this.linkLabel2.Location = new System.Drawing.Point(820, 278);
+            this.linkLabel2.Location = new System.Drawing.Point(811, 317);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(139, 20);
+            this.linkLabel2.Size = new System.Drawing.Size(185, 20);
             this.linkLabel2.TabIndex = 3;
             this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "Cotai Strip, Macau";
+            this.linkLabel2.Text = "Roomie Apartment Hotel";
             this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(780, 273);
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(771, 312);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(34, 25);
             this.label5.TabIndex = 6;
@@ -151,7 +158,8 @@ namespace HelloAuckland
             // 
             // label6
             // 
-            this.label6.Location = new System.Drawing.Point(781, 307);
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(772, 342);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(355, 84);
             this.label6.TabIndex = 4;
@@ -163,7 +171,8 @@ namespace HelloAuckland
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(332, 384);
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(317, 384);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(34, 25);
             this.label7.TabIndex = 6;
@@ -173,17 +182,18 @@ namespace HelloAuckland
             // 
             this.linkLabel3.AutoSize = true;
             this.linkLabel3.LinkColor = System.Drawing.Color.DimGray;
-            this.linkLabel3.Location = new System.Drawing.Point(379, 389);
+            this.linkLabel3.Location = new System.Drawing.Point(357, 389);
             this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(198, 20);
+            this.linkLabel3.Size = new System.Drawing.Size(273, 20);
             this.linkLabel3.TabIndex = 3;
             this.linkLabel3.TabStop = true;
-            this.linkLabel3.Text = "West Kowloon, Hong Kong";
+            this.linkLabel3.Text = "The Parnell Hotel & Conference Centre";
             this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // label8
             // 
-            this.label8.Location = new System.Drawing.Point(333, 420);
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(314, 419);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(326, 81);
             this.label8.TabIndex = 4;
@@ -195,7 +205,7 @@ namespace HelloAuckland
             // 
             this.button2.BackColor = System.Drawing.Color.MidnightBlue;
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(338, 513);
+            this.button2.Location = new System.Drawing.Point(309, 513);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(203, 46);
             this.button2.TabIndex = 5;
@@ -207,7 +217,7 @@ namespace HelloAuckland
             // 
             this.button3.BackColor = System.Drawing.Color.MidnightBlue;
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button3.Location = new System.Drawing.Point(776, 394);
+            this.button3.Location = new System.Drawing.Point(776, 429);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(212, 46);
             this.button3.TabIndex = 5;
@@ -217,8 +227,8 @@ namespace HelloAuckland
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Image = Image.FromFile("Images/Hotel2.jpg");  
-            this.pictureBox4.Location = new System.Drawing.Point(776, 38);
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(765, 81);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(291, 191);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -227,27 +237,27 @@ namespace HelloAuckland
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Image = Image.FromFile("Images/Hotel3.jpg");
-            this.pictureBox3.Location = new System.Drawing.Point(35, 343);
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 343);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(291, 191);
+            this.pictureBox3.Size = new System.Drawing.Size(291, 216);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 1;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = Image.FromFile("Images/Hotel1.jpg");
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 70);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(291, 191);
+            this.pictureBox1.Size = new System.Drawing.Size(291, 203);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = Image.FromFile("Images/HotelBackGround.png");
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(-16, -21);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(1177, 642);
@@ -261,11 +271,11 @@ namespace HelloAuckland
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label9.Location = new System.Drawing.Point(779, 241);
+            this.label9.Location = new System.Drawing.Point(770, 275);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(207, 32);
+            this.label9.Size = new System.Drawing.Size(324, 32);
             this.label9.TabIndex = 2;
-            this.label9.Text = "Galaxy Macau";
+            this.label9.Text = "ROMMIE APARTMENT";
             this.label9.Click += new System.EventHandler(this.label2_Click);
             // 
             // label10
@@ -273,11 +283,11 @@ namespace HelloAuckland
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label10.Location = new System.Drawing.Point(332, 343);
+            this.label10.Location = new System.Drawing.Point(316, 343);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(335, 32);
+            this.label10.Size = new System.Drawing.Size(257, 32);
             this.label10.TabIndex = 2;
-            this.label10.Text = "Ritz-Carlton Hong Kong";
+            this.label10.Text = "PARNELL HOTEL";
             this.label10.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form4
